4c0f319d3ae14ac0c1cab50390069f93a58f6508
Kevin Malomba <malombakevin@gmail.com>
2024-08-14 23:41:37 +0300

    darkmode text

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function DarkMode(){
 
     let body1 = document.body
-
     body1.classList.toggle('darkMode')
+
+    var btn = document.getElementById('sarah');
+    if(btn.innerText=="Darkmode"){
+       btn.innerText="light-mode";
+      }
+    else{
+      btn.innerText="Darkmode";
+      }
 
 }
